7063e2dd26f4cc03bf9976c24f3c64bbe3e55b55
jixatos <j.gody2004@gmail.com>
2023-03-24 08:45:56 -0300

    Calculadora simples usando match-case/if

---
+++ Exercício1_ex.py
@@ -0,0 +1,11 @@
+"""Dado um número, verificar se ele é maior que 100 e caso
+seja, o programa deve somar 150 a esse número. Ao final, o
+programa deve imprimir o resultado"""
+
+numero = int(input("Qual o número de sua escolha"))
+
+if numero > 100:
+    numero + 150
+
+print(numero)
+
