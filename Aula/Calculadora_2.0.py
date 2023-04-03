30e1282e5d3c19dc253bf8e40213c3a92f9effd7
jixatos <j.gody2004@gmail.com>
2023-04-03 14:14:58 -0300

    Espaçamento corrigido

--- Aula/Calculadora_2.0.py
+++ Aula/Calculadora_2.0.py
@@ -10,15 +10,15 @@ while True:
     if op>=1 and op <= 4:
         break
 
-    nun1 = float(input('Número 1: '))
-    nun2 = float(input('Número 2: '))
+nun1 = float(input('Número 1: '))
+nun2 = float(input('Número 2: '))
 
-    match op:
-        case 1:
-            print(f'{nun1} + {nun2}={nun1+nun2:.1f}')
-        case 2:
-            print(f'{nun1} - {nun2}={nun1-nun2:.1f}')
-        case 3:
-            print(f'{nun1} * {nun2}={nun1*nun2:.2f}')
-        case 4:
-            print(f'{nun1} / {nun2}={nun1/nun2:.2f}')
+match op:
+    case 1:
+         print(f'{nun1} + {nun2} = {nun1+nun2:.1f}')
+    case 2:
+        print(f'{nun1} - {nun2} = {nun1-nun2:.1f}')
+    case 3:
+        print(f'{nun1} * {nun2} = {nun1*nun2:.2f}')
+    case 4:
+        print(f'{nun1} / {nun2} = {nun1/nun2:.2f}')
