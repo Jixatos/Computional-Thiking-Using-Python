25df2bdbe0ee2bfb5c3c5f396c98b20aa0db7a94
jixatos <j.gody2004@gmail.com>
2023-04-10 10:36:26 -0300

    Inserção de variaveis

---
+++ Aula/Aula.py
@@ -0,0 +1,4 @@
+'''
+Escreva um programa  que receba como entrada os preços de itens comprados em uma loja por um cliente. Ao final, o pragrama deverá exibir o total da compra. Para informar que não há mais itens a serem comprados, o cliente deve digitar -1.
+'''
+
